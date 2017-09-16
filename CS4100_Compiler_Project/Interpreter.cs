573d451912c0bfb0d975c350451fca2c3b373160
Daniel F. Fosha <danfosha@gmail.com>
2017-09-16 14:58:56 -0600

    Completed interpeter per pseudocode in assignment

--- CS4100_Compiler_Project/Interpreter.cs
+++ CS4100_Compiler_Project/Interpreter.cs
@@ -9,7 +9,8 @@ namespace CS4100_Compiler_Project
     class Interpreter
     {
 
-        public int PC, OpCode, Op1, Op2, Op3 = 0;
+        public int PC;
+		public int OpCode, Op1, Op2, Op3 = 0; // don't need these if passing QuadTable in
         public int MaxQuad = 1000;
         public bool TraceOn;
         public SymbolTable SymbolTable;
@@ -22,7 +23,33 @@ namespace CS4100_Compiler_Project
         {
             while (PC < MaxQuad)
             {
-                
+                if (quadTable.OpCode<=16)
+				switch(QuadTable.OpCode)
+				{
+					case 0: // stop
+						Console.Writeline("Execution terminated by program stop");
+						PC = MaxQuad;
+						break;
+
+					case 1: // div
+						symbolTable[PC].Op3 = symbolTable[PC].Op1 / symbolTable[PC].Op2;
+						PC++1;
+						break;
+
+					case 8: // bnz
+						if (SymbolTable[PC].Op1 <> 0)
+						{
+							PC = Quadtable[PC].Op3
+						}
+						else
+						{
+							PC++1;
+						}
+						break;
+
+				}
+				
+
             }
 
         }
